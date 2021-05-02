44cce4d119372dc960e62979e7f5a805476db20c
Daniel3d <{ID}+{username}@users.noreply.github.com>
2021-05-01 20:58:36 -0700

    Se aplicacn buenas practicas de programcion, se crea el directorio src para poner todo ahi

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express'
 
 const loggerMiddleware = (req: Request, resp: Response, next: NextFunction) => {
-    if(Boolean(process.env.DEV)) {
+    if (Boolean(process.env.DEV)) {
         console.log('Request logged:', req.method, req.path);
     }
     // console.log('resp', resp.req.url );
